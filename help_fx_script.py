2828547a4c1866694bd119994163d474a9ae9945
a.kvinne <44692652+kvinne-anc@users.noreply.github.com>
2021-01-19 13:18:23 -0500

    My helper fx Prime

---
+++ help_fx_script.py
@@ -0,0 +1,11 @@
+## This is my first attempt at making a function
+# Trying to think of something useful
+# Prime numbers are useful
+
+import pandas
+
+from my_lambdata.helper_functions import Prime
+
+num = int(input("Enter a number: "))
+
+
